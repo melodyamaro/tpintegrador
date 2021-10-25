0d1cb3428ef743d754c0807e9674260411409cfe
unknown <melodyamaro@gmail.com>
2021-10-24 23:19:59 -0300

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function calcular()
+{ 
+    var seleccion=document.getElementById("categoria");
+    var seleccionValue=parseInt(seleccion.options[seleccion.selectedIndex].value);
+    var cantidad=document.getElementById("cantidad");
+
+    var total=0;
+
+    switch(seleccionValue)
+    {
+    case 1:
+
+    case 2:
+
+
+    case 3:
+
+           break;
+
+
+    }
+    document.getElementById("total")= total;
+
+}
